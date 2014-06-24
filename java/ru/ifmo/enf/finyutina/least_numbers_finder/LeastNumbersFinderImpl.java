f8ac15a2f7b509c462e9273c7ee48a04b26e4f99
iCharsi <charsi.npc@gmail.com>
2014-06-24 16:44:38 +0400

    Create LeastNumbersFinderImpl.java

---
+++ java/ru/ifmo/enf/finyutina/least_numbers_finder/LeastNumbersFinderImpl.java
@@ -0,0 +1,39 @@
+package ru.ifmo.enf.finyutina.least_numbers_finder;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * Created by Angelika Finyutina (charsi.npc@gmail.com) on 6/24/14.
+ */
+public class LeastNumbersFinderImpl implements LeastNumbersFinder {
+
+    private final PriorityQueue<Integer> leastK = new PriorityQueue<Integer>(1, Collections.reverseOrder());
+
+    public int[] find(int input[], int k) {
+
+        leastK.clear();
+
+        if (k < 0 || k > input.length) {
+            throw new IllegalArgumentException();
+        }
+        if (k == 0) {
+            return new int[0];
+        }
+
+        for (int i = 0; i < k; ++i) {
+            leastK.add(input[i]);
+        }
+        for (int i = k; i < input.length; ++i) {
+            leastK.add(input[i]);
+            leastK.poll();
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; ++i) {
+            result[k - 1 - i] = leastK.poll();
+        }
+
+        return result;
+    }
+}
