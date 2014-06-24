a34792fdfde1c4905a13f0cd9499340bbb5001f3
iCharsi <charsi.npc@gmail.com>
2014-06-24 16:43:37 +0400

    Create LeastNumbersFinder.java

---
+++ java/ru/ifmo/enf/finyutina/least_numbers_finder/LeastNumbersFinder.java
@@ -0,0 +1,8 @@
+package ru.ifmo.enf.finyutina.least_numbers_finder;
+
+/**
+ * Created by Angelika Finyutina (charsi.npc@gmail.com) on 6/24/14.
+ */
+public interface LeastNumbersFinder {
+    int[] find(int input[], int k);
+}
